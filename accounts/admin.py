5e2d88bac25ff2d2d0004660b87262772e0eccd9
fk-winner <hacklearn95@gmail.com>
2021-04-08 15:59:14 +0100

    completed category and account app

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,6 +5,9 @@ from .models import Account
 # Register your models here.
 class AccountAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'is_active', 'date_joined')
+    list_display_links = ('email', 'first_name', 'last_name')
+    readonly = ('last_login','date_joined')
+    ordering = ('-date_joined',)
     
     filter_horizontal = ()
     list_filter = ()
