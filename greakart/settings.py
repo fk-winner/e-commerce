69b234846e200b9db11dd0dd9b28744307d44538
fk-winner <hacklearn95@gmail.com>
2021-04-16 15:57:32 +0100

    django authentication is done

--- greakart/settings.py
+++ greakart/settings.py
@@ -132,3 +132,15 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP configure 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'hacklearn95@gmail.com'
+EMAIL_HOST_PASSWORD = 'fabricewinner'
+EMAIL_USE_TLS = True
