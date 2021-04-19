1d1779321647b7a422c4361b6ca7427db80c83f9
fk-winner <hacklearn95@gmail.com>
2021-04-19 10:17:44 +0100

    completed cart and checkout page for the logged in user

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-#Verication email 
+# Verication email
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -13,22 +13,28 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 
+from carts.views import _cart_id
+from carts.models import Cart, Cartitem
+import requests
+
 # Create your views here.
+
 
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            first_name      = form.cleaned_data['first_name']
-            last_name       = form.cleaned_data['last_name']
-            phone_number    = form.cleaned_data['phone_number']
-            email           = form.cleaned_data['email']
-            password        = form.cleaned_data['password']
-            username        = email.split('@')[0]
-            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0]
+            user = Account.objects.create_user(
+                first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-            
+
             # User activation
             current_site = get_current_site(request)
             mail_object = 'Veuillez activer votre compte'
@@ -55,23 +61,75 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
-        
+
         user = auth.authenticate(email=email, password=password)
-        
+
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = Cartitem.objects.filter(
+                    cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = Cartitem.objects.filter(cart=cart)
+
+                    # Geting the products variations by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # Get the cart item from the user to access his product variation
+                    cart_item = Cartitem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # product_variation = [1,2,3,4,5,6]
+                    # ex_var_list = [4,6,3,5]
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = Cartitem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = Cartitem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+
+            except:
+                print("Entrer à l'intérieur sauf bloc")
+                pass
             auth.login(request, user)
             messages.success(request, 'Vous êtes connecté')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                #next=/cart/checkout
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'identifiant de connexion invalide')
             return redirect('login')
     return render(request, 'accounts/login.html')
 
-@login_required(login_url = 'login')
+
+@login_required(login_url='login')
 def logout(request):
     auth.logout(request)
     messages.success(request, 'Vous êtes déconnecté')
     return redirect('login')
+
 
 def activate(request, uidb64, token):
     try:
@@ -79,7 +137,7 @@ def activate(request, uidb64, token):
         user = Account._default_manager.get(pk=uid)
     except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
         user = None
-    
+
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
@@ -90,17 +148,18 @@ def activate(request, uidb64, token):
         return redirect('register')
 
 
-@login_required(login_url = 'login')
+@login_required(login_url='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
+
 
 def forgotPassword(request):
     if request.method == 'POST':
         email = request.POST['email']
         if Account.objects.filter(email=email).exists():
             user = Account.objects.get(email__exact=email)
-            
-            #réinitialisation du mot de passe par courriel
+
+            # réinitialisation du mot de passe par courriel
             current_site = get_current_site(request)
             mail_object = 'Réinitialiser votre mot de passe'
             message = render_to_string('accounts/reset_password_email.html', {
@@ -112,14 +171,16 @@ def forgotPassword(request):
             to_email = email
             send_email = EmailMessage(mail_object, message, to=[to_email])
             send_email.send()
-            
-            messages.success(request, "L'e-mail de réinitialisation du mot de passe a été envoyé à votre adresse e-mail.")
+
+            messages.success(
+                request, "L'e-mail de réinitialisation du mot de passe a été envoyé à votre adresse e-mail.")
             return redirect('login')
-            
-        else: 
+
+        else:
             messages.error(request, "Le compte n'existe pas")
             return redirect('forgotPassword')
     return render(request, 'accounts/forgotPassword.html')
+
 
 def resetpassword_validate(request, uidb64, token):
     try:
@@ -127,7 +188,7 @@ def resetpassword_validate(request, uidb64, token):
         user = Account._default_manager.get(pk=uid)
     except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
         user = None
-    
+
     if user is not None and default_token_generator.check_token(user, token):
         request.session['uid'] = uid
         messages.success(request, 'Veuillez réinitialiser votre mot de passe')
@@ -136,21 +197,23 @@ def resetpassword_validate(request, uidb64, token):
         messages.error(request, 'Ce lien a expiré')
         return redirect('login')
 
+
 def resetPassword(request):
     if request.method == 'POST':
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
-        
+
         if password == confirm_password:
             uid = request.session.get('uid')
             user = Account.objects.get(pk=uid)
             user.set_password(password)
             user.save()
-            messages.success(request, 'Réinitialisation du mot de passe avec succès')
+            messages.success(
+                request, 'Réinitialisation du mot de passe avec succès')
             return redirect('login')
-            
+
         else:
             messages.error(request, 'Les mots de passe ne sont pas conformes')
             return redirect('resetPassword')
-    else:    
+    else:
         return render(request, 'accounts/resetPassword.html')
--- carts/context_processors.py
+++ carts/context_processors.py
@@ -8,8 +8,12 @@ def counter(request):
         return {}
     else:
         try:
-            cart = Cart.objects.filter(cart_id=_cart_id(request))
-            cart_items = Cartitem.objects.all().filter(cart=cart[:1])
+            cart = Cart.objects.filter(cart_id=_cart_id(request))  
+            if request.user.is_authenticated:
+                cart_items = Cartitem.objects.all().filter(user=request.user)
+            else:
+                cart_items = Cartitem.objects.all().filter(cart=cart[:1])
+                
             for cart_item in cart_items:
                 cart_count += cart_item.quantity
         except Cart.DoesNotExist:
